12f9781b3847e91b5810a529e504c408c12b7764
Jordan Byrd <jabyrd3@gmail.com>
2019-01-17 13:15:03 -0500

    clarify required str.replace

--- utils/packetbeat/install.sh
+++ utils/packetbeat/install.sh
@@ -1,6 +1,6 @@
 mkdir packetbeat
 curl -s https://github.com/ns1/ns1-privatedns/blob/master/utils/packetbeat/packetbeat.yml > packetbeat/packetbeat.yml
-echo 'please add the following service defintion to your docker-compose file
+echo 'please add the following service defintion to your docker-compose file. be sure to insert your api token and splunk-url'
   packetbeat:
     image: "docker.elastic.co/beats/packetbeat:6.5.4"
     net: host
@@ -9,8 +9,8 @@ echo 'please add the following service defintion to your docker-compose file
       - NET_ADMIN
     log_driver: splunk
     log_opt:
-      splunk-token: "fill this in appropriately"
-      splunk-url: "fill this in appropriately"
+      splunk-token: "<your splunk api token>"
+      splunk-url: "<your splunk url>"
       splunk-format: "json"
       tag: '{{ .ImageName }}/{{ .Name }}/{{ .ID }}'
     volumes:
