3b1d6e5ef9463d68a2e4ac71e1592c01ebfec320
Jordan Byrd <jabyrd3@gmail.com>
2019-01-18 14:04:05 -0500

    update log to take into account 3.2 syntax changes

--- utils/packetbeat/install.sh
+++ utils/packetbeat/install.sh
@@ -3,16 +3,17 @@ curl -s https://github.com/ns1/ns1-privatedns/blob/master/utils/packetbeat/packe
 echo 'please add the following service defintion to your docker-compose file. be sure to insert your api token and splunk-url'
   packetbeat:
     image: "docker.elastic.co/beats/packetbeat:6.5.4"
-    net: host
+    network_mode: host
     cap_add:
       - NET_RAW
       - NET_ADMIN
-    log_driver: splunk
-    log_opt:
-      splunk-token: "<your splunk api token>"
-      splunk-url: "<your splunk url>"
-      splunk-format: "json"
-      tag: '{{ .ImageName }}/{{ .Name }}/{{ .ID }}'
+    logging:
+      driver: splunk
+      options:
+        splunk-token: "<your splunk api token>"
+        splunk-url: "<your splunk url>"
+        splunk-format: "json"
+        tag: '{{ .ImageName }}/{{ .Name }}/{{ .ID }}'
     volumes:
       - "./packetbeat:/etc/packetbeat"
     command:
